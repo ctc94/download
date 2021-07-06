b0fab845e72031aa8e36fc2ec98294faa32883a2
ctc948040 <vagrant@default>
2021-07-06 17:56:07 +0900

    2021-07-06 17:56:06 modify files

---
+++ src/main/java/com/ctc/async/config/LogAspect.java
@@ -0,0 +1,30 @@
+package com.ctc.async.config;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StopWatch;
+
+@Configuration
+@Aspect
+public class LogAspect {
+	private static final Logger log = LoggerFactory.getLogger(LogAspect.class);
+	//@Around("execution(* * com.test.aop.controller.*(..))")
+	@Around("@annotation(LogExecutionTime)")
+	public Object executionAspect(ProceedingJoinPoint joinPoint) throws Throwable {
+		
+		String method = joinPoint.getSignature().getName();
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start(method);
+
+		Object result = joinPoint.proceed();
+		
+		stopWatch.stop();
+		log.info(stopWatch.prettyPrint()+"MS :"+stopWatch.getTotalTimeMillis()+"\nS:"+stopWatch.getTotalTimeSeconds());
+
+		return result;
+	}
+}
